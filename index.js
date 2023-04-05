10bbbc50b374a33755aa1e7d022f26b3daaa3b5b
unknown <parmarsinghbhanu@gmail.com>
2023-04-05 23:38:44 +0530

    link setup

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+dotenv.config();
+app.use(express.json());
+app.use(cors({
+    origin: [
+       "http://localhost:3000",
+    ],
+     credentials: true,
+   })
+);
 
+// connect to mongoDB
+  mongoose.set('strictQuery', false)
+  mongoose.connect(process.env.MDB_CONNECT) 
+  .then(()=>{console.log('Mongodb connected succesfully')});
+console.log("udsgfgd")
+
+
+//  set up routes
+// app.use("/", require("./Router/UserRouter"));
+
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+const path=require("path");
