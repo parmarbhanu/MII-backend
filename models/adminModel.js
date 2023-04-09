1b9e36b7a834b75d7514c7255cd02dd40ad46de5
unknown <parmarsinghbhanu@gmail.com>
2023-04-10 01:23:34 +0530

    login

--- models/adminModel.js
+++ models/adminModel.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
   username: String,
   email: String,
   phone: String,
+  pass:String
 });
 
 const User = mongoose.model('User', UserSchema);
--- router/adminRouter.js
+++ router/adminRouter.js
@@ -1,51 +1,71 @@
 const router = require("express").Router();
 const User = require('../models/adminModel');
+const bcrypt = require("bcryptjs");
 
-
-router.post("/user", async (req, res) => {
-  try {
-    const { name, email, phone} = req.body;
-    console.log(req.body);
-    if (!name || !email || !phone ) {
-      return res.status(400).json("all detail are required");
-    }
- // creating a new admin and save it to database  
-   const newUser = new User({
-      name,
-      email,
-      phone,
-    });
-    const savedUser = await newUser.save();
-   return res.status(200).json("user saved sucessfully")
-} 
-  catch {
-    return res.status(404).json("plz provide input");
-  }
-});
+// there is no need of creating and save new user API bcz ist a admin pannel login API
+// router.post("/user", async (req, res) => {
+//   try {
+//     const { username, email, phone,password} = req.body;
+//     console.log(req.body);
+//     if (!username || !email || !phone || !password) {
+//       return res.status(400).json("all detail are required");
+//     }
+// //  creating a new admin and save it to database 
+//      const salt = await bcrypt.genSalt();
+//     const pass = await bcrypt.hash(password, salt); 
+//    const newUser = new User({
+//       username,
+//       email,
+//       phone,
+//       pass,
+//     });
+//     const savedUser = await newUser.save();
+//    return res.status(200).json("user saved sucessfully")
+// } 
+//   catch {
+//     return res.status(404).json("plz provide input");
+//   }
+// });
 
 // log in admin
-
-router.post("/login", async (req, res) => {
+router.get("/login", async (req, res) => {
   try {
-    const { phoneno, password } = req.body;
-    if (!phoneno || !password)
+    const { email, password } = req.body;
+    
+    if (!email || !password)
       return res
         .status(400)
         .json({ errorMessage: "Please enter all required fields." });
 
-    const existingUser = await User.findOne({ phoneno });
+    const existingUser = await User.findOne({ email });
     if (!existingUser)
-      return res.status(401).json({ errorMessage: "Wrong email or password." });
+      return res.status(401).json({ errorMessage: "Wrong cradentials." });
 
-    const passwordCorrect = await bcrypt.compare(
-      password,
-      existingUser.passwordHash
-    );
+    const passwordCorrect = await bcrypt.compare(password,existingUser.password);
     if (!passwordCorrect)
       return res.status(401).json({ errorMessage: "Wrong email or password." });
-      
+   
+      // sign the token
+
+    // const token = jwt.sign(
+    //   {
+    //     user: existingUser._id,
+    //   },
+    //   process.env.JWT_SECRET
+    // );
+
+    // send the token in a HTTP-only cookie
+
+    // res
+    //   .cookie("token", token, {
+    //     httpOnly: true,
+    //     secure: true,
+    //     sameSite: "none",
+    //   })
+    //   .send();
   } catch (err) {
-    res.status(200).send(err);
+    console.error(err);
+    res.status(400).send(err);
   }
 });
 
