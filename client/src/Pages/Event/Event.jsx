e8f54a97e9fefbf85830890bdac52ebcc2f4d39b
unknown <parmarsinghbhanu@gmail.com>
2023-04-30 22:11:12 +0530

    blog aa gaya he

--- client/src/Pages/Event/Event.jsx
+++ client/src/Pages/Event/Event.jsx
@@ -28,85 +28,6 @@ const apicall=async ()=>{
         </div>
         {openform?<AddEvent />:<></>}
         
-      {/* </div> */}
-      <div className="events">
-        <div className="event-box">
-          <div className="event-img">
-            <img src="https://cdn.pixabay.com/photo/2017/07/21/23/57/concert-2527495_1280.jpg" alt="event1" />
-          </div>
-          <div className="event-discription">
-            <h3 className="title">Title</h3>
-            <p className="event-txt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
-            <i className="fa fa-trash  event-delete-btn" aria-hidden="true"/>
-          </div>
-        </div>
-        <div className="event-box">
-          <div className="event-img">
-            <img src="https://cdn.pixabay.com/photo/2016/08/16/09/53/international-conference-1597531__480.jpg" alt="eventyy" />
-          </div>
-          <div className="event-discription">
-            <h3 className="title">Title</h3>
-            <p className="event-txt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
-            <div className="event-row">
-              <div className='date-time'><span>20/4/2023</span><span> 6:60PM</span></div>
-              <p>Location</p>
-            </div>
-            <div className="event-row">
-              <span>Number of speaker: 5</span>
-              <span>500/-</span>
-            </div>
-            <i className="fa fa-trash event-delete-btn" aria-hidden="true"/>
-          </div>
-        </div>
-        <div className="event-box">
-          <div className="event-img">img</div>
-          <div className="event-discription">
-            <h3 className="title">Title</h3>
-            <p className="event-txt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
-            <div className="event-row">
-              <div className='date-time'><span>20/4/2023</span><span> 6:60PM</span></div>
-              <p>Location</p>
-            </div>
-            <div className="event-row">
-              <span>Number of speaker: 5</span>
-              <span>500/-</span>
-            </div>
-            <i className="fa fa-trash  event-delete-btn" aria-hidden="true"/>
-          </div>
-        </div>
-        <div className="event-box">
-          <div className="event-img">img</div>
-          <div className="event-discription">
-            <h3 className="title">Title</h3>
-            <p className="event-txt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
-            <div className="event-row">
-              <div className='date-time'><span>20/4/2023</span><span> 6:60PM</span></div>
-              <p>Location</p>
-            </div>
-            <div className="event-row">
-              <span>Number of speaker: 5</span>
-              <span>500/-</span>
-            </div>
-            <i className="fa fa-trash  event-delete-btn" aria-hidden="true"/>
-          </div>
-        </div>
-        <div className="event-box">
-          <div className="event-img">img</div>
-          <div className="event-discription">
-            <h3 className="title">Title</h3>
-            <p className="event-txt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
-            <div className="event-row">
-              <div className='date-time'><span>20/4/2023</span><span> 6:60PM</span></div>
-              <p>Location</p>
-            </div>
-            <div className="event-row">
-              <span>Number of speaker: 5</span>
-              <span>500/-</span>
-            </div>
-            <i className="fa fa-trash  event-delete-btn" aria-hidden="true"/>
-          </div>
-        </div>
-      </div>
       </div>
       <div className="blogs">
         <p>Events</p>
@@ -119,3 +40,98 @@ const apicall=async ()=>{
 }
 
 export default Event
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // {/* </div> */}
+    //   {/* <div className="events">
+    //     <div className="event-box">
+    //       <div className="event-img">
+    //         <img src="https://cdn.pixabay.com/photo/2017/07/21/23/57/concert-2527495_1280.jpg" alt="event1" />
+    //       </div>
+    //       <div className="event-discription">
+    //         <h3 className="title">Title</h3>
+    //         <p className="event-txt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
+    //         <i className="fa fa-trash  event-delete-btn" aria-hidden="true"/>
+    //       </div>
+    //     </div>
+    //     <div className="event-box">
+    //       <div className="event-img">
+    //         <img src="https://cdn.pixabay.com/photo/2016/08/16/09/53/international-conference-1597531__480.jpg" alt="eventyy" />
+    //       </div>
+    //       <div className="event-discription">
+    //         <h3 className="title">Title</h3>
+    //         <p className="event-txt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
+    //         <div className="event-row">
+    //           <div className='date-time'><span>20/4/2023</span><span> 6:60PM</span></div>
+    //           <p>Location</p>
+    //         </div>
+    //         <div className="event-row">
+    //           <span>Number of speaker: 5</span>
+    //           <span>500/-</span>
+    //         </div>
+    //         <i className="fa fa-trash event-delete-btn" aria-hidden="true"/>
+    //       </div>
+    //     </div>
+    //     <div className="event-box">
+    //       <div className="event-img">img</div>
+    //       <div className="event-discription">
+    //         <h3 className="title">Title</h3>
+    //         <p className="event-txt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
+    //         <div className="event-row">
+    //           <div className='date-time'><span>20/4/2023</span><span> 6:60PM</span></div>
+    //           <p>Location</p>
+    //         </div>
+    //         <div className="event-row">
+    //           <span>Number of speaker: 5</span>
+    //           <span>500/-</span>
+    //         </div>
+    //         <i className="fa fa-trash  event-delete-btn" aria-hidden="true"/>
+    //       </div>
+    //     </div>
+    //     <div className="event-box">
+    //       <div className="event-img">img</div>
+    //       <div className="event-discription">
+    //         <h3 className="title">Title</h3>
+    //         <p className="event-txt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
+    //         <div className="event-row">
+    //           <div className='date-time'><span>20/4/2023</span><span> 6:60PM</span></div>
+    //           <p>Location</p>
+    //         </div>
+    //         <div className="event-row">
+    //           <span>Number of speaker: 5</span>
+    //           <span>500/-</span>
+    //         </div>
+    //         <i className="fa fa-trash  event-delete-btn" aria-hidden="true"/>
+    //       </div>
+    //     </div>
+    //     <div className="event-box">
+    //       <div className="event-img">img</div>
+    //       <div className="event-discription">
+    //         <h3 className="title">Title</h3>
+    //         <p className="event-txt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
+    //         <div className="event-row">
+    //           <div className='date-time'><span>20/4/2023</span><span> 6:60PM</span></div>
+    //           <p>Location</p>
+    //         </div>
+    //         <div className="event-row">
+    //           <span>Number of speaker: 5</span>
+    //           <span>500/-</span>
+    //         </div>
+    //         <i className="fa fa-trash  event-delete-btn" aria-hidden="true"/>
+    //       </div>
+    //     </div> */}
+    //   {/* </div> */}
