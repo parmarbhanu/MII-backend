4cc17a470ce7d1d2fe2ac5b2cde52351e7c3a924
unknown <parmarsinghbhanu@gmail.com>
2023-04-30 21:59:09 +0530

    trying to make working

--- client/src/Pages/Blog/Blog.jsx
+++ client/src/Pages/Blog/Blog.jsx
@@ -1,14 +1,57 @@
-// import React, { useEffect, useState } from 'react'
-// import './Blog.css'
-// import Singleblog from './Singleblog'
-// import axios from "axios"
+import React, { useEffect, useState } from 'react'
+import './Blog.css'
+import Singleblog from './Singleblog'
+import AddBlog from './AddBlog';
+import axios from "axios"
+
+const Blog = () => {
+const [openform,setopenform]=useState(false);
+const [sign,setSign] =useState('plus');
+const signChange = () => {
+  console.log("openform = " +openform+ " sign = " + sign);
+  openform? setSign('plus') : setSign('xmark');
+}
+
+
+const [blogs, setblogs] = useState([]);
+const apicall=async ()=>{
+    await axios.get("/blog/allblogs").then((res) => { setblogs(res.data) });
+  }
+
+  useEffect(() => {
+    apicall();
+  }, [])
+  return (
+    <div>
+      <div className="blog-component">
+        <p>Blogs</p>
+        <div className="add-icon" id='add-icon-blog' onClick={()=>{setopenform(!openform);signChange()}}>
+            <i className={`fa-solid fa-${sign}`}/>
+        </div>
+
+        {openform?<AddBlog />:<></>}
+        <div className="blogs">  
+
+        </div>
+      </div>
+      <div className="blogs">
+        <p>Blogs</p>
+         {blogs.map((item) => {
+         return <Singleblog id={item._id} title={item.title} description={item.description} images={item.images} authername={item.authername} date={item.date}/>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Blog
+
 
 
 
 
 
 // const Blog = () => {
-
 //   const [blogs, setblogs] = useState([]);
 //   const apicall=async ()=>{
 //     await axios.get("/blog/allblogs").then((res) => { setblogs(res.data) });
@@ -32,6 +75,8 @@
 //             <button type="submit" id='blog-txt-add'>Add</button>
 //             <button type="reset">Clear</button>
 //           </form>
+
+
 //         </div>
 //       </div>
 //       <div className="blogs">
@@ -47,139 +92,107 @@
 // export default Blog
 
 
-// import React from 'react'
-import React, { useEffect, useState } from 'react'
-import AddBlog from './AddBlog';
-import './Blog.css'
-// import { useState } from 'react'
-import Singleblog from './Singleblog'
-import axios from "axios"
 
-const Blog = () => {
-const [openform,setopenform]=useState(false);
-const [sign,setSign] =useState('plus');
-const signChange = () => {
-  console.log("openform = " +openform+ " sign = " + sign);
-  openform? setSign('plus') : setSign('xmark');
-}
-const [blogs, setblogs] = useState([]);
-const apicall=async ()=>{
-    await axios.get("/blog/allblogs").then((res) => { setblogs(res.data) });
-  }
-  useEffect(() => {
-    apicall();
-  }, [])
-  return (
-    <div>
-      <div className="blog-component">
-        <p>Blogs</p>
-        <div className="add-icon" id='add-icon-blog' onClick={()=>{setopenform(!openform);signChange()}}>
-            {/* icon of add */}
-            <i className={`fa-solid fa-${sign}`}/>
-        </div>
 
-        {openform?<AddBlog />:<></>}
-        <div className="blogs">
-          <div className="blog-box">
-            <div className="blog-img">
-              <img src="https://cdn.pixabay.com/photo/2017/01/18/08/25/social-media-1989152__480.jpg" alt="blog1" />
-            </div>
-            <div className="blog-content">
-              <div className="row1">
-                <h3 className="title">Title</h3>
-                <p className="blogtxt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
-              </div>
-              <div className='row-2'>
-                <span className="publisher">XYZ</span> 
-                <span className="date">10/4/2023</span>
-              </div>
-              <div className="btn-section row-3">          
-                <button className='delete-btn'>Delete</button>
-              </div> 
-            </div>
-          </div>
-          <div className="blog-box">
-            <div className="blog-img">
-              <img src="https://cdn.pixabay.com/photo/2017/01/18/08/25/social-media-1989152__480.jpg" alt="blog1" />
-            </div>
-            <div className="blog-content">
-              <div className="row1">
-                <h3 className="title">Title</h3>
-                <p className="blogtxt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
-              </div>
-              <div className='row-2'>
-                <span className="publisher">XYZ</span> 
-                <span className="date">10/4/2023</span>
-              </div>
-              <div className="btn-section row-3">          
-                <i className="fa fa-trash delete-btn" aria-hidden="true"/>
-              </div> 
-            </div>
-          </div>
-          <div className="blog-box">
-            <div className="blog-img">
-              <img src="https://cdn.pixabay.com/photo/2017/01/18/08/25/social-media-1989152__480.jpg" alt="blog1" />
-            </div>
-            <div className="blog-content">
-              <div className="row1">
-                <h3 className="title">Title</h3>
-                <p className="blogtxt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
-              </div>
-              <div className='row-2'>
-                <span className="publisher">XYZ</span> 
-                <span className="date">10/4/2023</span>
-              </div>
-              <div className="btn-section row-3">          
-                <i className="fa fa-trash delete-btn" aria-hidden="true"/>
-              </div> 
-            </div>
-          </div>
-          <div className="blog-box">
-            <div className="blog-img">
-              <img src="https://cdn.pixabay.com/photo/2017/01/18/08/25/social-media-1989152__480.jpg" alt="blog1" />
-            </div>
-            <div className="blog-content">
-              <div className="row1">
-                <h3 className="title">Title</h3>
-                <p className="blogtxt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
-              </div>
-              <div className='row-2'>
-                <span className="publisher">XYZ</span> 
-                <span className="date">10/4/2023</span>
-              </div>
-              <div className="btn-section row-3">          
-                <i className="fa fa-trash delete-btn" aria-hidden="true"/>
-              </div> 
-            </div>
-          </div>
-          <div className="blog-box">
-            <div className="blog-img">
-              <img src="https://cdn.pixabay.com/photo/2017/01/18/08/25/social-media-1989152__480.jpg" alt="blog1" />
-            </div>
-            <div className="blog-content">
-              <div className="row1">
-                <h3 className="title">Title</h3>
-                <p className="blogtxt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
-              </div>
-              <div className='row-2'>
-                <span className="publisher">XYZ</span> 
-                <span className="date">10/4/2023</span>
-              </div>
-              <div className="btn-section row-3">          
-                <i className="fa fa-trash delete-btn" aria-hidden="true"/>
-              </div> 
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="blogs">
-        <p>Blogs</p>
-         {blogs.map((item) => {
-         return <Singleblog id={item._id} title={item.title} description={item.description} images={item.images} authername={item.authername} date={item.date}/>
-        })}
-      </div>
-    </div>
-  )
-}
 
-export default Blog
+
+
+
+
+
+
+
+
+
+
+
+// {/* <div className="blog-box">
+//             <div className="blog-img">
+//               <img src="https://cdn.pixabay.com/photo/2017/01/18/08/25/social-media-1989152__480.jpg" alt="blog1" />
+//             </div>
+//             <div className="blog-content">
+//               <div className="row1">
+//                 <h3 className="title">Title</h3>
+//                 <p className="blogtxt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
+//               </div>
+//               <div className='row-2'>
+//                 <span className="publisher">XYZ</span> 
+//                 <span className="date">10/4/2023</span>
+//               </div>
+//               <div className="btn-section row-3">          
+//                 <button className='delete-btn'>Delete</button>
+//               </div> 
+//             </div>
+//           </div> */}
+//           {/* <div className="blog-box">
+//             <div className="blog-img">
+//               <img src="https://cdn.pixabay.com/photo/2017/01/18/08/25/social-media-1989152__480.jpg" alt="blog1" />
+//             </div>
+//             <div className="blog-content">
+//               <div className="row1">
+//                 <h3 className="title">Title</h3>
+//                 <p className="blogtxt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
+//               </div>
+//               <div className='row-2'>
+//                 <span className="publisher">XYZ</span> 
+//                 <span className="date">10/4/2023</span>
+//               </div>
+//               <div className="btn-section row-3">          
+//                 <i className="fa fa-trash delete-btn" aria-hidden="true"/>
+//               </div> 
+//             </div>
+//           </div> */}
+//           {/* <div className="blog-box">
+//             <div className="blog-img">
+//               <img src="https://cdn.pixabay.com/photo/2017/01/18/08/25/social-media-1989152__480.jpg" alt="blog1" />
+//             </div>
+//             <div className="blog-content">
+//               <div className="row1">
+//                 <h3 className="title">Title</h3>
+//                 <p className="blogtxt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
+//               </div>
+//               <div className='row-2'>
+//                 <span className="publisher">XYZ</span> 
+//                 <span className="date">10/4/2023</span>
+//               </div>
+//               <div className="btn-section row-3">          
+//                 <i className="fa fa-trash delete-btn" aria-hidden="true"/>
+//               </div> 
+//             </div>
+//           </div> */}
+//           {/* <div className="blog-box">
+//             <div className="blog-img">
+//               <img src="https://cdn.pixabay.com/photo/2017/01/18/08/25/social-media-1989152__480.jpg" alt="blog1" />
+//             </div>
+//             <div className="blog-content">
+//               <div className="row1">
+//                 <h3 className="title">Title</h3>
+//                 <p className="blogtxt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
+//               </div>
+//               <div className='row-2'>
+//                 <span className="publisher">XYZ</span> 
+//                 <span className="date">10/4/2023</span>
+//               </div>
+//               <div className="btn-section row-3">          
+//                 <i className="fa fa-trash delete-btn" aria-hidden="true"/>
+//               </div> 
+//             </div>
+//           </div> */}
+//           {/* <div className="blog-box">
+//             <div className="blog-img">
+//               <img src="https://cdn.pixabay.com/photo/2017/01/18/08/25/social-media-1989152__480.jpg" alt="blog1" />
+//             </div>
+//             <div className="blog-content">
+//               <div className="row1">
+//                 <h3 className="title">Title</h3>
+//                 <p className="blogtxt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, aspernatur cupiditate sit voluptatem obcaecati porro tempora sed molestias corporis modi optio libero animi.</p>
+//               </div>
+//               <div className='row-2'>
+//                 <span className="publisher">XYZ</span> 
+//                 <span className="date">10/4/2023</span>
+//               </div>
+//               <div className="btn-section row-3">          
+//                 <i className="fa fa-trash delete-btn" aria-hidden="true"/>
+//               </div> 
+//             </div>
+//           </div> */}
